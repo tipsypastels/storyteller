000cec96cd99155a7d4425c5d32f82b0370da3f1
Dakota Sankey <dakotasankey@gmail.com>
2022-03-12 18:22:28 -0800

    test database

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,11 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
+
+const DATABASE_URL = Deno.env.get("DATABASE_URL")!;
+
+const pool = new postgres.Pool(DATABASE_URL, /* size */ 3, /* lazy */ true);
+
+await pool.connect();
 
 console.log("Listening on http://localhost:8000");
 serve((_req) => {
